a0123460cc2f1067d7ca9b3c1ba7f5c2c5da768e
Shpak Aliaksandr <shpak-aliaksandr@outlook.com>
2018-10-05 20:48:50 +0300

    1

--- ResourceEditor/App_Start/BundleConfig.cs
+++ ResourceEditor/App_Start/BundleConfig.cs
@@ -8,22 +8,32 @@ namespace ResourceEditor
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/node_modules/jquery/dist/jquery.js"));
+            /*JS*/
+            Bundle jqueryBundle = new ScriptBundle("~/bundles/jquery");
+            jqueryBundle.Include("~/node_modules/jquery/dist/jquery.js");
+            jqueryBundle.CdnFallbackExpression = @"~/node_modules/jquery/dist/jquery.js";
+            jqueryBundle.CdnPath = @"https://code.jquery.com/jquery-3.3.1.js";
+            bundles.Add(jqueryBundle);
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/node_modules/jquery-validation/dist/jquery.validate.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryvalunob").Include("~/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/node_modules/bootstrap/dist/js/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/main").Include("~/Scripts/main.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/Kendo").Include("~/node_modules/kendo-ui-core/js/kendo.core.js"));
+            Bundle kendoUIBundle = new ScriptBundle("~/bundles/kendoUI");
+            kendoUIBundle.CdnPath = @"http://kendo.cdn.telerik.com/2014.1.318/js/kendo.all.min.js";
+            kendoUIBundle.Include("~/Scripts/kendo/2014.1.318/kendo.web.min.js");
+            bundles.Add(kendoUIBundle);
 
-            bundles.Add(new ScriptBundle("~/bundles/main").Include("~/Scripts/main.js")); 
-
+            /*CSS*/
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/node_modules/bootstrap/dist/css/bootstrap.css"));
 
-            bundles.Add(new StyleBundle("~/Content/Kendo").Include("~/node_modules/kendo-ui-core/css/web/kendo.default.css"));
+            bundles.Add(new StyleBundle("~/Content/kendoUI").Include(
+                "~/Content/kendo/2014.1.318/kendo.common.min.css",
+                "~/Content/kendo/2014.1.318/kendo.bootstrap.min.css"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/site").Include("~/Content/site.css"));
-
         }
     }
 }
