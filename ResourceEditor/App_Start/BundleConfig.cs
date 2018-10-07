f0665a8d24b1b455c5bb0dab955a799c72a34379
Shpak Aliaksandr <shpak-aliaksandr@outlook.com>
2018-10-08 01:22:26 +0300

    1

--- ResourceEditor/App_Start/BundleConfig.cs
+++ ResourceEditor/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace ResourceEditor
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;   //enable CDN support
+
             /*JS*/
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery", @"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js").Include(
                 "~/node_modules/jquery/dist/jquery.js",
                 "~/node_modules/jquery.cookie/jquery.cookie.js")
                 );
@@ -22,20 +24,18 @@ namespace ResourceEditor
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/node_modules/bootstrap/dist/js/bootstrap.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/main").Include("~/Scripts/main.js"));
+             
+            bundles.Add(new Bundle("~/bundles/main").Include("~/Scripts/main.js")); //неверная минификация
 
             /*CSS*/
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
                 "~/node_modules/bootstrap/dist/css/bootstrap.css"));
 
-            bundles.Add(new StyleBundle("~/Content/kendoUI").Include(
-                "~/node_modules/kendo/2014.1.318/css/kendo.common.min.css",
-                "~/node_modules/kendo/2014.1.318/css/kendo.bootstrap.min.css"
-                ));
-
             bundles.Add(new StyleBundle("~/Content/site").Include(
                 "~/Content/site.css"));
+
+            // Code removed for clarity.
+            //BundleTable.EnableOptimizations = false;
         }
     }
 }
