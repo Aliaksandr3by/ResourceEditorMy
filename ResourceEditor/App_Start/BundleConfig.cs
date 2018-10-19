08c679c85bf0d9caf66440342d10d22c95005877
Shpak Aliaksandr <shpak-aliaksandr@outlook.com>
2018-10-19 19:55:16 +0300

    1

--- ResourceEditor/App_Start/BundleConfig.cs
+++ ResourceEditor/App_Start/BundleConfig.cs
@@ -8,12 +8,8 @@ namespace ResourceEditor
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.UseCdn = true;   //enable CDN support
-
-            bundles.Add(new ScriptBundle("~/bundles/materialize-css").Include(
-                "~/node_modules/materialize-css/dist/js/materialize.js"));
-
             /*JS*/
+
             bundles.Add(new ScriptBundle("~/bundles/jquery", @"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js").Include(
                 "~/node_modules/jquery/dist/jquery.js",
                 "~/node_modules/jquery.cookie/jquery.cookie.js"));
@@ -24,18 +20,15 @@ namespace ResourceEditor
             bundles.Add(new ScriptBundle("~/bundles/jqueryvalunob").Include(
                 "~/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/node_modules/bootstrap/dist/js/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                "~/node_modules/materialize-css/dist/js/materialize.js"));
 
             bundles.Add(new Bundle("~/bundles/main").Include("~/src/main.js")); //неверная минификация
 
             /*CSS*/
 
-            bundles.Add(new StyleBundle("~/Content/materialize-css").Include(
+            bundles.Add(new StyleBundle("~/Content/materialize").Include(
                 "~/node_modules/materialize-css/dist/css/materialize.css"));
-
-            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
-                "~/node_modules/bootstrap/dist/css/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/site").Include(
                 "~/Content/site.css"));
